cb317f371b74f5b5b824088f46cd18cd381dcc12
Davi da Silva Böger <dsboger@gmail.com>
2014-09-28 00:39:01 -0300

    Added some description about the installed files

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/sh
 
-install -D -m755 zimsearch.py ${DESTDIR}/usr/bin/
-install -D -m644 br.com.dsboger.zimsearch.provider.service ${DESTDIR}/usr/share/dbus-1/services/
-install -D -m644 br.com.dsboger.zimsearch-search-provider.ini ${DESTDIR}/usr/share/gnome-shell/search-providers/
-desktop-file-install --dir=${DESTDIR}/usr/share/applications -m 644 br.com.dsboger.zimsearch.desktop
+# python script that starts DBUS service
+install -D -m755 zimsearch.py "${DESTDIR}/usr/bin/"
+
+# DBUS service descriptor
+install -D -m644 br.com.dsboger.zimsearch.provider.service "${DESTDIR}/usr/share/dbus-1/services/"
+
+# GNOME Shell search provider descriptor
+install -D -m644 br.com.dsboger.zimsearch-search-provider.ini "${DESTDIR}/usr/share/gnome-shell/search-providers/"
+
+# ZimSearch desktop file. NoDisplay and DBusActivatable
+desktop-file-install --dir="${DESTDIR}/usr/share/applications" -m 644 br.com.dsboger.zimsearch.desktop
+
